1f56bf8c93e4490f0ed5b61eb7e6568b34a5a9cb
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2018-06-07 15:04:32 +0430

    Complete FwdUnit implementation

--- ForwardingUnit.v
+++ ForwardingUnit.v
@@ -1,10 +1,16 @@
 module ForwardingUnit (EXE_MEM_RegWr, MEM_WB_RegWr, MEM_WB_rd,
   MEM_WB_MemRd, EXE_MEM_MemWr, ID_EXE_rs, ID_EXE_rt, EXE_MEM_rd, MemForward,
-  forward);
+  forward1, forward2);
 
   input EXE_MEM_RegWr, MEM_WB_RegWr, MEM_WB_MemRd, EXE_MEM_MemWr;
   input[2:0] MEM_WB_rd, ID_EXE_rs, ID_EXE_rt, EXE_MEM_rd;
-  output forward, MemForward;
+  output[1:0] forward1, forward2
+  output MemForward;
 
+  assign MemForward = (EXE_MEM_MemWr && MEM_WB_MemRd) && (MEM_WB_rd == EXE_MEM_rd) ? 1 : 0;
+  assign forward1 = ((ID_EXE_rs == EXE_MEM_rd) && (EXE_MEM_RegWr)) ? 2'b10 :
+    (MEM_WB_RegWr && (MEM_WB_rd == ID_EXE_rs) && ((EXE_MEM_rd != ID_EXE_rs) || EXE_MEM_RegWr == 0) ? 2'b01 : 2'b00);
+  assign forward2 = ((ID_EXE_rt == EXE_MEM_rd) && (EXE_MEM_RegWr)) ? 2'b10 :
+    (MEM_WB_RegWr && (MEM_WB_rd == ID_EXE_rt) && ((EXE_MEM_rd != ID_EXE_rt) || EXE_MEM_RegWr == 0) ? 2'b01 : 2'b00);
   
 endmodule // ForwardingUnit
