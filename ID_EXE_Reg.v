b555977a91b7dd38f33d3fdb35d6ece170dea5dd
GkHabib <h.ghadimi1998@gmail.com>
2018-06-09 03:22:16 +0430

    Fix ","

--- ID_EXE_Reg.v
+++ ID_EXE_Reg.v
@@ -1,7 +1,7 @@
 module ID_EXE_Reg (clk, rst, regWr_IN, memRd_IN, memWr_IN, aluOp_IN, cWr_IN, zWr_IN,
   immConst_IN, rd_IN, rs_IN, rt_IN, regData1_IN, regData2_IN, brDisp_IN, pcPlus1_IN,
   regWr_OUT, memRd_OUT, memWr_OUT, aluOp_OUT, cWr_OUT, zWr_OUT, immConst_OUT,
-  rd_OUT, rs_OUT, rt_OUT, regData1_OUT, regData2_OUT, brDisp_OUT, pcPlus1_OUT,);
+  rd_OUT, rs_OUT, rt_OUT, regData1_OUT, regData2_OUT, brDisp_OUT, pcPlus1_OUT);
 
   input clk, rst, regWr_IN, memRd_IN, memWr_IN, cWr_IN, zWr_IN;
   input[2:0] rd_IN, rs_IN, rt_IN;
