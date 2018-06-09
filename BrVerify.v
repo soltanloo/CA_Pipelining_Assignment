9b61bafa03ba1acb1fe002ef66ed5b79b9ba68d6
GkHabib <h.ghadimi1998@gmail.com>
2018-06-09 20:01:39 +0430

    Fix lack of default case in case statement

--- BrVerify.v
+++ BrVerify.v
@@ -9,6 +9,7 @@ module BrVerify (opCode, Cin, Zin, BrTaken);
     5'b10101 : begin if(~Zin)begin BrTaken=1; end end
     5'b10110 : begin if(Cin)begin BrTaken=1; end  end
     5'b10111 : begin if(~Cin)begin BrTaken=1; end end
+    default: BrTaken=0;
     endcase
   end
 endmodule // BrVerify
