b1c25649aa022db875f9218c1593b62d73138877
GkHabib <h.ghadimi1998@gmail.com>
2018-06-09 15:28:55 +0430

    Fix ";"

--- mux8bit3to1.v
+++ mux8bit3to1.v
@@ -4,9 +4,9 @@ module mux8bit3to1 (in1, in2, in3, sel, out);
   output reg [7:0] out;
   always @ (in1, in2, in3, sel) begin
     case(sel)
-    0:begin out = in1 end
-    1:begin out = in2 end
-    2:begin out = in3 end
+    0:begin out = in1; end
+    1:begin out = in2; end
+    2:begin out = in3; end
     endcase
   end
 endmodule // mux8bit3to1
