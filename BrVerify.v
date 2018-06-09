a6024a4f12abac41a453030ce6ce6c08ad2c8897
GkHabib <h.ghadimi1998@gmail.com>
2018-06-09 19:54:01 +0430

    Add branch verifier module

---
+++ BrVerify.v
@@ -0,0 +1,14 @@
+module BrVerify (opCode, Cin, Zin, BrTaken);
+  input [4:0] opCode;
+  input Cin, Zin;
+  output reg BrTaken;
+  always @ ( opCode, Cin, Zin ) begin
+    BrTaken=0;
+    case(opCode)
+    5'b10100 : begin if(Zin)begin BrTaken=1; end  end
+    5'b10101 : begin if(~Zin)begin BrTaken=1; end end
+    5'b10110 : begin if(Cin)begin BrTaken=1; end  end
+    5'b10111 : begin if(~Cin)begin BrTaken=1; end end
+    endcase
+  end
+endmodule // BrVerify
