84141978dedb44112c4b78750eb89ca12f12760a
GkHabib <h.ghadimi1998@gmail.com>
2018-06-09 20:24:26 +0430

    Fix immConst signal

--- ID_EXE_Reg.v
+++ ID_EXE_Reg.v
@@ -3,17 +3,17 @@ module ID_EXE_Reg (clk, rst, regWr_IN, memRd_IN, memWr_IN, aluOp_IN, cWr_IN, zWr
   regWr_OUT, memRd_OUT, memWr_OUT, aluOp_OUT, cWr_OUT, zWr_OUT, immConst_OUT,
   rd_OUT, rs_OUT, rt_OUT, regData1_OUT, regData2_OUT, brDisp_OUT, pcPlus1_OUT, opCode_IN, opCode_OUT);
 
-  input clk, rst, regWr_IN, memRd_IN, memWr_IN, cWr_IN, zWr_IN;
+  input clk, rst, regWr_IN, memRd_IN, memWr_IN, cWr_IN, zWr_IN, immConst_IN;
   input[2:0] rd_IN, rs_IN, rt_IN;
   input[3:0] aluOp_IN;
   input[4:0] opCode_IN;
-  input[7:0] immConst_IN, regData1_IN, regData2_IN, brDisp_IN;
+  input[7:0] regData1_IN, regData2_IN, brDisp_IN;
   input[11:0] pcPlus1_IN;
-  output reg regWr_OUT, memRd_OUT, memWr_OUT, cWr_OUT, zWr_OUT;
+  output reg regWr_OUT, memRd_OUT, memWr_OUT, cWr_OUT, zWr_OUT, immConst_OUT;
   output reg[2:0] rd_OUT, rs_OUT, rt_OUT;
   output reg[3:0] aluOp_OUT;
   output reg[4:0] opCode_OUT;
-  output reg[7:0] immConst_OUT, regData1_OUT, regData2_OUT, brDisp_OUT;
+  output reg[7:0]  regData1_OUT, regData2_OUT, brDisp_OUT;
   output reg[11:0] pcPlus1_OUT;
 
   always @(posedge clk, posedge rst) begin
