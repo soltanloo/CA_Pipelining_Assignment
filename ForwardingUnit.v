ef96ed730a2dbf34f6e4abbb1e4d72da5d1de828
GkHabib <h.ghadimi1998@gmail.com>
2018-06-09 15:43:19 +0430

    Fix ";"

--- ForwardingUnit.v
+++ ForwardingUnit.v
@@ -4,7 +4,7 @@ module ForwardingUnit (EXE_MEM_RegWr, MEM_WB_RegWr, MEM_WB_rd,
 
   input EXE_MEM_RegWr, MEM_WB_RegWr, MEM_WB_MemRd, EXE_MEM_MemWr;
   input[2:0] MEM_WB_rd, ID_EXE_rs, ID_EXE_rt, EXE_MEM_rd;
-  output[1:0] forward1, forward2
+  output[1:0] forward1, forward2;
   output MemForward;
 
   assign MemForward = (EXE_MEM_MemWr && MEM_WB_MemRd) && (MEM_WB_rd == EXE_MEM_rd) ? 1 : 0;
@@ -12,5 +12,5 @@ module ForwardingUnit (EXE_MEM_RegWr, MEM_WB_RegWr, MEM_WB_rd,
     (MEM_WB_RegWr && (MEM_WB_rd == ID_EXE_rs) && ((EXE_MEM_rd != ID_EXE_rs) || EXE_MEM_RegWr == 0) ? 2'b01 : 2'b00);
   assign forward2 = ((ID_EXE_rt == EXE_MEM_rd) && (EXE_MEM_RegWr)) ? 2'b10 :
     (MEM_WB_RegWr && (MEM_WB_rd == ID_EXE_rt) && ((EXE_MEM_rd != ID_EXE_rt) || EXE_MEM_RegWr == 0) ? 2'b01 : 2'b00);
-  
+
 endmodule // ForwardingUnit
